c90f4c7f3ba89212eaa93c54c27253afaff4f0b3
MikelosR <miiikelossspap@outlook.com.gr>
2025-08-11 17:49:15 +0300

    Remove some comments

--- back/src/main/java/com/seax/back/repository/UserRepository.java
+++ back/src/main/java/com/seax/back/repository/UserRepository.java
@@ -9,21 +9,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     //1. Find user by email (for login)
-    //Spring generates: SELECT * FROM users WHERE email = ?
     User  findByEmail(String email);
-    //2. For getting user by ID (when i will add tokens later)
-    //findById() is already provided by JpaRepository!
-    //All other methods (save, delete, findById, etc.) come FREE from JpaRepository
+
 }
-
-/*When you write extends JpaRepository<User, Long>, Spring automatically gives me 20+ methods:
-
-java// Spring automatically provides these methods:
-User save(User user);                    // ← This is where .save() comes from!
-Optional<User> findById(Long id);        // ← This too
-List<User> findAll();                    // ← And this
-void delete(User user);                  // ← And this
-void deleteById(Long id);               // ← And this
-long count();                           // ← And this
-boolean existsById(Long id);            // ← And this
-// ... and 15+ more methods!*/
